3d62b94d75dccd027c90dd869b5672945a080ec1
Ebirien Nte <e_b_i_67@yahoo.com>
2024-01-13 15:50:48 +0000

    Adds question_4.sql file

--- question_4.sql
+++ question_4.sql
@@ -1,7 +1,7 @@
-SELECT s.store_type As "Store Type", 
-ROUND(CAST(SUM(o.product_quantity * p.sale_price) AS NUMERIC), 2) AS "Total Sales"
-, 100 * SUM(o.product_quantity * p.sale_price) /SUM(SUM(o.product_quantity * p.sale_price)) OVER()  AS "Percentage Total Sales"
-, COUNT(o.order_date_uuid) AS Order_Count 
+SELECT s.store_type As "Store Type"
+, ROUND(CAST(SUM(o.product_quantity * p.sale_price) AS NUMERIC), 2) AS "Total Sales"
+, ROUND(CAST(100 * SUM(o.product_quantity * p.sale_price) /SUM(SUM(o.product_quantity * p.sale_price)) OVER() AS NUMERIC), 2)  AS "Percentage Total Sales"
+, COUNT(o.order_date_uuid) AS "Order Count" 
 FROM orders AS o
 INNER JOIN dim_product AS p ON
 o.product_code = p.product_code
